8e9fdb787faf8513740732ba272df16eee22722a
Ichinose Yoshihiro <maruisansmai@gmail.com>
2025-05-20 16:02:14 +0900

    tmp

--- app/src/main/java/io/github/mikan/tart/article/ArticleStoreContainer.kt
+++ app/src/main/java/io/github/mikan/tart/article/ArticleStoreContainer.kt
@@ -7,41 +7,5 @@ import javax.inject.Inject
 class ArticleStoreContainer @Inject constructor(
     private val articleRepository: ArticleRepository
 ) {
-    fun build(itemId: String): Store<ArticleState, ArticleAction, ArticleEvent> =
-        Store(ArticleState.Idle) {
-            state<ArticleState.Idle> {
-                enter {
-                    nextState(ArticleState.Loading)
-                }
-            }
-            state<ArticleState.Loading> {
-                enter {
-                    val articleDetail =
-                        articleRepository.getArticle(itemId)?.toArticleDetail()
-                    val isLiked = articleRepository.isItemLiked(itemId)
-                    val isStocked = articleRepository.isItemStock(itemId)
-                    if (articleDetail != null) {
-                        nextState(
-                            ArticleState.Success(
-                                detail = articleDetail,
-                                isLiked = isLiked,
-                                isStocked = isStocked,
-                            )
-                        )
-                    } else {
-                        nextState(ArticleState.Error("Article not found"))
-                    }
-                }
-            }
-            state<ArticleState.Success> {
-                action<ArticleUiAction.ClickBack> {
-                    event(ArticleEvent.NavigateBack)
-                }
-            }
-            state<ArticleState> {
-                error<Exception> {
-                    nextState(ArticleState.Error(error.message ?: "Unknown error"))
-                }
-            }
-        }
+    fun build(itemId: String): Store<ArticleState, ArticleAction, ArticleEvent> = TODO()
 }
